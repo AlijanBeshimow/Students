9346b672aee28fefcc1211a18e72e6d83254e443
Alijan Beshimow <beshimowalijan@gmail.com>
2024-01-17 19:42:11 +0200

    Delete

--- app.py
+++ app.py
@@ -4,18 +4,35 @@ app = Flask(__name__)
 
 
 class Contact:
-    def __init__(self):
-        pass
+    def __init__(self, contact_tuple):
+        self.id = contact_tuple[0]
+        self.name = contact_tuple[1]
+        self.phone = contact_tuple[2]
+        self.email = contact_tuple[3]
+        self.address = contact_tuple[4]
+        self.user = contact_tuple[5]
+
+
+def query(sql):
+    with sqlite3.connect("contacts.db") as conn:
+        cur = conn.cursor()
+        rows = cur.execute(sql)
+        return [Contact(row) for row in rows.fetchall()]
 
 
 def get_contacts():
     with sqlite3.connect("contacts.db") as conn:
         cur = conn.cursor()
         rows = cur.execute("SELECT * FROM contacts")
-        print(rows)
-        return rows.fetchall()
+        return [Contact(row) for row in rows.fetchall()]
 
 
 @app.route('/')
 def home():
     return render_template("home.html", contacts=get_contacts())
+
+
+@app.route('/delete')
+def delete():
+    query(f"DELETE FROM contacts WHERE id={request.args['id']}")
+    return home()
