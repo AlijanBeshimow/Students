1d19942515463c56d4a5aea4f901e8c29cfa5d9f
Alijan Beshimow <beshimowalijan@gmail.com>
2024-01-21 17:51:06 +0200

    Some changes

--- test_data.py
+++ test_data.py
@@ -6,7 +6,7 @@ def add_contacts():
     with sqlite3.connect("contacts.db") as conn:
         cur = conn.cursor()
         for i in range(20):
-            profile = Faker(locale="he_IL").simple_profile()
-            phone = Faker(locale="he_IL").phone_number()
+            profile = Faker().simple_profile()
+            phone = Faker().phone_number()
             cur.execute(
                 f"INSERT INTO contacts (name, phone, email, address) VALUES ('{profile["name"]}','{phone}','{profile["mail"]}','{profile["address"]}')")
